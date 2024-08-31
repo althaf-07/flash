57a19c5c5fdd5dec1d3ecd0eff664fb34b6bbebd
Althaf Muhammad <flashlib0308@gmail.com>
2024-08-31 07:55:46 +0000

    Import key functions from modules to make them accessible at the package level

--- flash/__init__.py
+++ flash/__init__.py
@@ -1,3 +1,12 @@
+# Importing key functions from modules to make them accessible at the package level
+from .analysis import (
+    stats_moments, hist_box_viz, nan_value_viz, count_viz, pair_viz, corr_heatmap_viz, 
+    crosstab_heatmap_viz, violin_point_viz, feature_transform_viz
+    )
+from .preprocessing import (
+    extract_features, find_outliers, calc_na_values
+    )
+
 # Metadata
 __version__ = '0.1.0'
 __author__ = 'Althaf Muhammad'
